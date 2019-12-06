33e205e7a4cce1705c342b43779679f112abbc24
Damian Perez <Damian@laboratorios.df>
2019-12-06 15:51:17 -0300

    correcion de sintaxis y agrefado de comentarios en vacio.m

--- Vacio/vacio.m
+++ Vacio/vacio.m
@@ -2,102 +2,78 @@ close all;
 clear all;
 instrreset;
 %%
-%
-presion = serial('COM14','BaudRate',9600,'DataBits',8,'StopBits',1,'Parity','none', 'terminator','CR') ;
 
-multimetro = serial('COM16','BaudRate',9600,'DataBits',8,'StopBits',1,'Parity','none');
+% Crear y configurar la interfaz con el sensor de presión
+interfazPesion = serial('COM14','BaudRate',9600,'DataBits',8,'StopBits',1,'Parity','none', 'terminator','CR') ;
 
+% Crear y configurar la interfaz con el sensor de presión
+interfazMultimetro = serial('COM16','BaudRate',9600,'DataBits',8,'StopBits',1,'Parity','none');
 set(multimetro,'terminator','CR','InputBufferSize',15,'ReadAsyncMode','manual');
 
-fopen(presion);
+% Abrir la comunicacion con el multimetro y el sensor de presión
+fopen(interfazPesion);
+fopen(interfazMultimetro);
 
-fopen(multimetro);
 %% MEDICION SIMPLES
+
+% El script amprobe38XRA(interfaz, verbose) se encarga de comunicarse con el multimetro y pedirle la ultima medicion.
+% 	@interfaz es la interfaz serial con el multimetro
+%	@verbose: si es distinto de 0, el script muestra por pantalla la informacion que devuelve el multimetro
 verbose=0;
-[Ylab , valueMultimetro, str, count] = amprobe38XRA(multimetro, verbose);
+[Ylab , valueMultimetro, str, count] = amprobe38XRA(interfazMultimetro, verbose);
+
 fprintf('Valor: %g %s \n',valueMultimetro,Ylab);
 
-strpresion = query(presion,'?GA1'); 
+% El enviando la cadena ?GAx, el medidor de vacio devuelve un string con la presion del sensor conectado a la salida "x"
+% de la electronica.
+% 	Ej: query(interfaz,'?GA1') devuelve la presion del sensor conectado a la salida gauge 1 
+strpresion = query(interfazPesion,'?GA1'); 
+
 %derecha esta midiendo sobre el platino
 %izquierda esta midiendo sobre la R de 1 ohm
 
 %26 de junio medimos 
 
-M = 3000;
-pausa = 2;
-nombre = 'enfriamientodifusora';
+cantidadDeMediciones = 3000;		% Numero total de mediciones
+pausaEntreMediciones = 2;			% Pausa en segundos entre que termina una medicion y comienza la siguiente
+nombre = 'enfriamientodifusora';	% Nombre del archivo donde se van a guardar los datos
+
 % NumeroTotal_Proceso_Descripcion_NumeroParcial
 
-Tiempo =  zeros(1,M);
-Temperatura = zeros(1,M);
-Presion = zeros(1,M);
-tic
+tiempo =  zeros(1,cantidadDeMediciones);
+temperatura = zeros(1,cantidadDeMediciones);
+presion = zeros(1,cantidadDeMediciones);
 
-archivo_de_datos = strcat(nombre,'.txt');       %crea el nombre del archivo de datos
-FILE = fopen(archivo_de_datos,'at');
-fprintf(FILE,'%s\n','$ Medicion  ;  Tiempo   ;  Presion ; Temperatura ' ); %agregar presion
-% fprintf(FILE,'%s\n',' pausa = ');
-% fprintf(FILE,'%f', pausa);
+archivo_de_datos = strcat(nombre,'.txt');       %crea el nombre del archivo de datos con su extensiónn
+FILE = fopen(archivo_de_datos,'at');			%crea el archivo
+fprintf(FILE,'%s \n','$ Medicion; Tiempo; Presion; Temperatura' ); %Escribe el encabezado del archivo
+
+tic												%iniciar cronometro					
+for indice = 1:cantidadDeMediciones
+    fprintf('Medicion: %d de %d.\n', indice, cantidadDeMediciones)
+	tiempo(indice) = toc;											%Medicion de tiempo del cronometro
+	presion(indice) = str2double(query(interfazPesion,'?GA1')) ;	%pide la presion y transforma el string que recibe a double
+    [Ylab , valueMultimetro, str, count] = amprobe38XRA(interfazMultimetro, verbose); %pide una medicion al mulktimetro
+    temperatura(indice) = valueMultimetro;							%guarda el valor del multimetro en la variable temperatura
+	figure(1);
+	subplot(2,1,1);													%crear una figura con 2 subplot de 2 filas y una columna y
+																	%graficar en la figura de arriba 
+	plot(tiempo(1:indice), presion(1:indice), '.')
+	subplot(2,1,2); 												%crear una figura con 2 subplot de 2 filas y una columna y
+																	%graficar en la figura de abajo 
+	plot(Tiempo(1:indice), temperatura(1:indice), '.')
 
 
+	fprintf(FILE, '%d ;', indice);									%guardar los valores obtenidos en el archivo
+	fprintf(FILE, '%f ;', tiempo(indice));
+	fprintf(FILE, '%f ;', presion(indice));
+	fprintf(FILE, '%f \n', temperatura(indice));
 
-
-for k = 1:M
-    disp('midiendo')
-    Tiempo(k)=toc;
-    Presion(k)=str2double(query(presion,'?GA1')) ;
-    [Ylab , valueMultimetro, str, count] = amprobe38XRA(multimetro,verbose);
-    Temperatura(k)= valueMultimetro;
-%       drawnow
-       figure(1);
-       plot(Tiempo(1:k),Presion(1:k), '.')
-       figure(2);
-       plot(Tiempo(1:k),Temperatura(1:k), '.')
-       %  figure(3);
-      % plot(Presion(1:k), VoltajeIzquierda(1:k)./VoltajeDerecha(1:k))
-       fprintf(FILE,'%1.0d',k);
-       fprintf(FILE,'%s',' ; ');
-       fprintf(FILE,'%f',Tiempo(k));
-       fprintf(FILE,'%s',' ; ');
-       fprintf(FILE,'%f',Presion(k));
-       fprintf(FILE,'%s',' ; ');       
-       fprintf(FILE,'%f',Temperatura(k));
-       fprintf(FILE,'\n');       
-       disp(k)
-       %save(archivo_de_datos,'-append', k, Tiempo(k), Presion(k), Temperatura(k))
-       pause(pausa);
+	pause(pausaEntreMediciones);									%hacer una pausa antes de la proxima medición
 end
 disp('Ya termino')
 fclose(FILE);
 
-%plot(TIME',DATA')
 
-
-% csvwrite('tiempo.txt',Tiempo)
-% csvwrite('presion.txt',Presion)
-%'C:\Users\publico.LABORATORIOS\Desktop\grupo 5\matlabScripts\Presion19dejunio80mA.dat','Presion','-ascii','-double')
-% csvwrite('temperatura.txt',Temperatura)
-%'C:\Users\publico.LABORATORIOS\Desktop\grupo 5\matlabScripts\VoltajeDerecha19dejunio80mA.dat','VoltajeDerecha','-ascii','-double')
-
-
-
-
-%otras cosas que se pueden medir
-% MEASure
-%  :VOLTage:DC? {<range>|MIN|MAX|DEF},{<resolution>|MIN|MAX|DEF}
-%  :VOLTage:DC:RATio? {<range>|MIN|MAX|DEF},{<resolution>|MIN|MAX|DEF}
-%  :VOLTage:AC? {<range>|MIN|MAX|DEF},{<resolution>|MIN|MAX|DEF}
-%  :CURRent:DC? {<range>|MIN|MAX|DEF},{<resolution>|MIN|MAX|DEF}
-%  :CURRent:AC? {<range>|MIN|MAX|DEF},{<resolution>|MIN|MAX|DEF}
-%  :RESistance? {<range>|MIN|MAX|DEF},{<resolution>|MIN|MAX|DEF}
-%  :FRESistance? {<range>|MIN|MAX|DEF},{<resolution>|MIN|MAX|DEF}
-%  :FREQuency? {<range>|MIN|MAX|DEF},{<resolution>|MIN|MAX|DEF}
-%  :PERiod? {<range>|MIN|MAX|DEF},{<resolution>|MIN|MAX|DEF}
-%  :CONTinuity?
-%  :DIODe?
-
-
-%% cierra la sesión con mm
-fclose(multimetro);
-fclose(presion);
-%delete(multimetro);
+fclose(interfazMultimetro);
+fclose(interfazPesion);
